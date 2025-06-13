f0c7c2cdf7c959f4fec26e139f6a31a4bf1b6c45
Felipe Parrondo Bastos <feliparrondobastos@gmail.com>
2025-06-13 06:11:44 -0300

    added repository annotation to sample

--- id2/src/main/java/com/example/id2/repository/PatientRepository.java
+++ id2/src/main/java/com/example/id2/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package com.example.id2.repository;
 
 import com.example.id2.model.PatientModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository extends MongoRepository<PatientModel, String> {
 }
